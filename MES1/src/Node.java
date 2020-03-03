2fec0d587b6fac81c4e8f787cd6f8b0cc850f030
klaudiaawronaa <43877735+klaudiaawronaa@users.noreply.github.com>
2020-03-03 14:29:18 +0100

    Update Node.java

--- MES1/src/Node.java
+++ MES1/src/Node.java
@@ -1,21 +1,20 @@
 public class Node {
     double x;
     double y;
-    //double t;
     int id;
 
 
-    public Node (double x, double y, int id)
-    {   this.id=id;
-        this.x=x;
-        this.y=y;
+    public Node(double x, double y, int id) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
     }
 
-    public double getX(){
+    public double getX() {
         return x;
     }
 
-    public double getY(){
+    public double getY() {
         return y;
     }
 
