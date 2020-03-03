c2d4bd74c577e8eced85bc48459a8675e7a69016
klaudiaawronaa <43877735+klaudiaawronaa@users.noreply.github.com>
2020-03-03 14:19:42 +0100

    Update Grid.java

--- MES1/src/Grid.java
+++ MES1/src/Grid.java
@@ -1,16 +1,15 @@
 public class Grid {
     int tmp_nH;
     int tmp_nL;
-    int ne=(tmp_nL-1)*(tmp_nH-1);
-    int nh=tmp_nL*tmp_nH;
+    int ne = (tmp_nL - 1) * (tmp_nH - 1);
+    int nh = tmp_nL * tmp_nH;
     Element[] elementGrid;
     Node[] nodeGrid;
 
-    public Grid(int nL, int nH, Element[] element, Node[] node)
-    {
-        tmp_nH=nH;
-        tmp_nL=nL;
-        elementGrid=element;
-        nodeGrid=node;
+    public Grid(int nL, int nH, Element[] element, Node[] node) {
+        tmp_nH = nH;
+        tmp_nL = nL;
+        elementGrid = element;
+        nodeGrid = node;
     }
 }
